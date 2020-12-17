05a72a01817b6b1f19f53945844c6933386ee091
AdamWeissman <adam@comedytragedyepic.com>
2020-12-17 17:40:11 -0500

    webServer with middleware setup

--- createWebServer/index.js
+++ createWebServer/index.js
@@ -1,1 +1,59 @@
-console.log("hello!")
+const express = require('express');
+const app = express(); // this tells our webserver everything it can do
+const bodyParser = require('body-parser') // THIS IS TO REPLACE THE CUSTOM BODY PARSER WRITTEN BELOW
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
+// ROUTE HANDLERS BELOW
+app.get('/', (request, response) => {
+  response.send(`
+  <br>
+  <br>
+  <br>
+  <center>
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" /><br>
+        <input name="pw" placeholder="password" /><br>
+        <input name="pwConfirmation" placeholder="confirm password" /><br>
+        <button>SIGN UP</button
+      </form>
+    </div>
+  </center>
+  `)
+});
+
+// THE CODE BELOW IS REPLACED BY REQUIRE STATEMENT ABOVE
+// const bodyParser = (request, response, next) => {
+//   if (request.method === 'POST') {
+//     request.on('data', data => {
+//       const parsed = data.toString('utf8').split('&') 
+//       const formData = {};
+//       for (let pair of parsed) {
+//         const [key, value] = pair.split('='); //some destructuring, first val is key, second val is value
+//         formData[key] = value
+//       }
+//       request.body = formData
+//       next()
+//     });
+//   } else {
+//     next(); 
+//   }
+// }
+
+// THIS IS ADDED LOCALLY... prior to adding bodyParser being added globally...
+// app.post('/', bodyParser.urlencoded({ extended: true }), (request, response) => { //bodyParser is a middleware
+//   console.log(request.body)
+//   response.send('ACCOUNT CREATED!!!')
+// })
+
+app.post('/', (request, response) => { //bodyParser is globally applied with app.use on line 4
+  console.log(request.body)
+  response.send('ACCOUNT CREATED!!!')
+})
+
+
+app.listen(3000, () => {
+  console.log('Listening')
+})
+
--- createWebServer/webServerNotes.md
+++ createWebServer/webServerNotes.md
@@ -1,5 +1,6 @@
 # PROJECT SETUP
 
+## BASICS
 1) Run `npm init -y` to get a package.json
 2) common installs express and nodemon...  
   `npm install express nodemon` 
@@ -8,4 +9,37 @@
  "scripts": {
     "dev": "nodemon index.js"
   },
+```  
+4) `npm run dev` runs `nodemon index.js`
+
+## WEB SERVER SETUP (cont'd from above...)
+
+5) The basic setup is like so... 
+```
+const express = require('express');
+const app = express(); // this tells our webserver everything it can do
+
+// ROUTE HANDLERS BELOW
+app.get('/', (request, response) => { //GET IS THE METHOD REQUEST
+  response.send(` // must use backticks
+      <form method="POST"> //method changed to POST 
+        <input name="email" placeholder="email" /><br>
+        <input name="pw" placeholder="password" /><br>
+        <input name="pwConfirmation" placeholder="confirm password" /><br>
+        <button>SIGN UP</button
+      </form>
+  `) // make sure to use backticks to include HTML
+});
+
+
+
+app.listen(3000, () => { // NOTE THAT 3000 could be whatever host you want
+  console.log('Listening')
+})
+```
+
+6) FOR HTML PARSING... MAKE SURE TO INCLUDE THE FOLLOWING MIDDLEWARE ALONG WITH THE OTHER REQUIRES...
+```
+  const bodyParser = require('body-parser') // THIS IS TO REPLACE THE CUSTOM BODY PARSER WRITTEN BELOW
+  app.use(bodyParser.urlencoded({ extended: true}));
 ```
