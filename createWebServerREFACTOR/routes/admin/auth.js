7680551a5165c0c98a1d10328ede93874eb40129
AdamWeissman <adam@comedytragedyepic.com>
2020-12-19 12:57:52 -0500

    transferred routes from index.js to auth.js

---
+++ createWebServerREFACTOR/routes/admin/auth.js
@@ -0,0 +1,85 @@
+
+
+app.get('/signup', (request, response) => {
+  response.send(`
+  <br>
+  <br>
+  <br>
+  <center>
+    <div>
+    YOUR ID IS: ${request.session.someUserID}
+      <form method="POST">
+        <input name="email" placeholder="email" /><br>
+        <input name="pw" placeholder="password" /><br>
+        <input name="pwConfirm" placeholder="confirm password" /><br>
+        <button>SIGN UP</button
+      </form>
+    </div>
+  </center>
+  `)
+});
+
+app.post('/signup', async (request, response) => { //bodyParser is globally applied with app.use on line 4
+  const { email, pw, pwConfirm } = request.body
+  
+  const existingUser = await usersRepo.getOneBy( { email: email }); //can use  { email } instead because the name is the same as the value 
+  if (existingUser) {
+    return response.send('EMAIL IN USE');
+  }
+
+  if (pw !== pwConfirm) {
+    return response.send('PASSWORDS MUST MATCH');
+  }
+
+
+  const user = await usersRepo.create({ email, pw});
+
+  request.session.someUserID = user.id // this added by the cookieSession
+
+  response.send('ACCOUNT CREATED!')
+})
+
+app.get('/signout', (request, response) => {
+  request.session = null
+  response.send("You are logged out");
+})
+
+app.get('/signin', async (request, response) => {
+  response.send(`
+  <br>
+  <br>
+  <br>
+  <center>
+    <div>
+    YOUR ID IS: ${request.session.someUserID}
+      <form method="POST">
+        <input name="email" placeholder="email" /><br>
+        <input name="pw" placeholder="password" /><br>
+        <button>SIGN IN</button
+      </form>
+    </div>
+  </center>
+  `);
+})
+
+app.post('/signin', async (request, response) => {
+  const { email, pw } = request.body; //LOVE THAT DESTRUCTURING!!
+
+  const user = await usersRepo.getOneBy({ email })  // { email } is equivalent to { email: email}
+
+  if (!user) {
+    return response.send("EMAIL NOT FOUND")
+  } 
+
+  const validPassword = await usersRepo.comparePasswords(
+    user.pw,
+    pw
+  )
+
+  if (!validPassword) {
+    return response.send('INVALID PASSWORD');
+  }
+
+  request.session.someUserID = user.id;
+  response.send("YOU ARE NOW LOGGED IN")
+});
