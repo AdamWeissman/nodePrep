838991918f2d7415b5822dafd17c2b88892a3281
AdamWeissman <adam@comedytragedyepic.com>
2020-12-19 19:36:38 -0500

    plugging away

--- createWebServerREFACTOR/routes/admin/auth.js
+++ createWebServerREFACTOR/routes/admin/auth.js
@@ -4,7 +4,7 @@ const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
-const { requireEmail } = require('./validators')
+const { requireEmail, requirePassword, requirePasswordConfirmation } = require('./validators')
 
 const router = express.Router();
 
@@ -16,33 +16,16 @@ router.post(
   '/signup',
   [ 
     requireEmail,
-    
-    check('password')
-      .trim()
-      .isLength({ min: 4, max: 20 }),
-    check('passwordConfirmation')
-      .trim()
-      .isLength({ min: 4, max: 20 })
+    requirePassword,
+    requirePasswordConfirmation
   ],
   async (req, res) => {
     const errors = validationResult(req);
     console.log(errors);
 
     const { email, password, passwordConfirmation } = req.body;
-
-    const existingUser = await usersRepo.getOneBy({ email });
-    if (existingUser) {
-      return res.send('Email in use');
-    }
-
-    if (password !== passwordConfirmation) {
-      return res.send('Passwords must match');
-    }
-
-    // Create a user in our user repo to represent this person
     const user = await usersRepo.create({ email, password });
 
-    // Store the id of that user inside the users cookie
     req.session.userId = user.id;
 
     res.send('Account created!!!');
--- createWebServerREFACTOR/routes/admin/validators.js
+++ createWebServerREFACTOR/routes/admin/validators.js
@@ -1,5 +1,5 @@
 const { check } = require('express-validator')
-const usersRepo = require('../../repositories')
+const usersRepo = require('../../repositories/users')
 
 module.exports = {
   requireEmail: check('email')
@@ -12,5 +12,18 @@ module.exports = {
         if (existingUser) {
           throw new Error('Email in use');
         }
-    })
+    }),
+    requirePassword: check('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('must be between 4 and 20 characters'),
+    requirePasswordConfirmation: check('passwordConfirmation')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('must also be between 4 and 20... and matching')
+      .custom((passwordConfirmation, { req }) => {
+        if (passwordConfirmation !== req.body.password) {
+          throw new Error('passwords must match')
+        }
+      }) 
 };
